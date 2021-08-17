9f40efd237e35c9062ddfc21f909dd6f325934d2
Yuliia Sokolova <Yuliia.Sokolova@halliburton.com>
2021-08-17 08:05:59 +0300

    STAGE_test_PJR/stage_settings_db_vs_azure.py

--- DEV_test_ETL/settings_for_etl.py
+++ DEV_test_ETL/settings_for_etl.py
@@ -10,7 +10,8 @@ password = 'Halliburton1'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+#connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+connect_str = "DefaultEndpointsProtocol=https;AccountName=storageaccountpepepba38;AccountKey=rMNwWiGzF2qlQOOY/4KTNXlkRFBe3BUD2Jnxf7sme+nlLsQy84QVttaol55SV+oP/J/KeFjxYPaD2nv26MS/dg==;EndpointSuffix=core.windows.net"
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
 local_path = "data"
--- STAGE_test_PJR/stage_settings_db_vs_azure.py
+++ STAGE_test_PJR/stage_settings_db_vs_azure.py
@@ -9,7 +9,9 @@ password = 'Halliburton1'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-stg_connect_str = os.getenv('STAGE_AZURE_STORAGE_CONNECTION_STRING')
+#stg_connect_str = os.getenv('STAGE_AZURE_STORAGE_CONNECTION_STRING')
+stg_connect_str = "DefaultEndpointsProtocol=https;AccountName=storageaccountpepep9765;AccountKey=gr2XHdMUhXPTtq2V5NW19dZGs3LsP2ZHl7dYah/yV2XgvbID/SXg8ji8UfbFsdKErTWLnZY+1iFF+B2zKrGT9A==;EndpointSuffix=core.windows.net"
+service = BlobServiceClient.from_connection_string(conn_str=stg_connect_str)
 blob_service_client = BlobServiceClient.from_connection_string(stg_connect_str)
 
 local_path = "stage_data"
