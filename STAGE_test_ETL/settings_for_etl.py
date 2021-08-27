961df366754dc458d120b62c7eb87a57ba9f13cf
Yuliia Sokolova <Yuliia.Sokolova@halliburton.com>
2021-08-27 16:13:56 +0300

    STAGE_test_ETL

--- STAGE_test_ETL/settings_for_etl.py
+++ STAGE_test_ETL/settings_for_etl.py
@@ -2,7 +2,6 @@ import pyodbc
 from azure.storage.blob import BlobServiceClient
 import os
 
-
 server = 'fracvaultstagingdbserver.database.windows.net'
 database = 'DR_Testing'
 username = 'fracvaultsql'
@@ -10,7 +9,6 @@ password = 'Halliburton1'
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
 
-#stg_connect_str = os.getenv('STAGE_AZURE_STORAGE_CONNECTION_STRING')
 stg_connect_str = "DefaultEndpointsProtocol=https;AccountName=storageaccountpepepb62e;AccountKey=ryg/iMmlzyjJR6SAtuhzshJ04MAt4MD0dU3ZwL8m4ZQPZWW0SCMFqgHfESe/OY9g/Ob3sYkHWM3Hy2AH8RpVYw==;EndpointSuffix=core.windows.net"
 service = BlobServiceClient.from_connection_string(conn_str=stg_connect_str)
 blob_service_client = BlobServiceClient.from_connection_string(stg_connect_str)
